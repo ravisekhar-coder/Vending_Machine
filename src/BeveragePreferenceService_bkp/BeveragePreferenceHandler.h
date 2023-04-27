70dacc97d13a86269b1e383d25ca1099942a3290
ravisekhar-coder <raviseshu536@gmail.com>
2023-04-26 23:07:58 -0400

    First Commit

---
+++ src/BeveragePreferenceService_bkp/BeveragePreferenceHandler.h
@@ -0,0 +1,44 @@
+#ifndef VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCEHANDLER_H
+#define VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCEHANDLER_H
+
+#include <iostream>
+#include <string>
+#include <regex>
+#include <future>
+
+#include "../../gen-cpp/BeveragePreferenceService.h"
+
+#include "../ClientPool.h"
+#include "../ThriftClient.h"
+#include "../logger.h"
+
+namespace vending_machine{
+
+class BeveragePreferenceServiceHandler : public BeveragePreferenceServiceIf {
+ public:
+  BeveragePreferenceServiceHandler();
+  ~BeveragePreferenceServiceHandler() override=default;
+
+  BeverageType::type GetBeverage(BeverageType btype) override;
+};
+
+// Constructor
+BeveragePreferenceServiceHandler::BeveragePreferenceServiceHandler() {
+
+}
+
+// Remote Procedure "PlaceOrder"
+
+BeverageType::type BeveragePreferenceServiceHandler::GetBeverage(const int64_t city) {
+     // Your implementation goes here
+     printf("GetBeveragePreference\n");
+
+    // randomly select a weather
+    return (0 == city%2)? BeverageType::type::cappuccino : BeverageType::type::lemonade;
+}
+
+} // namespace vending_machine
+
+
+#endif //VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCEHANDLER_H
+
